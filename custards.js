ba6acc483ab3f7ec565ae581399bcb94c67040f3
Harika Punna <harika.punna@accelerite.com>
2017-11-22 12:11:03 +0530

    Modal and Image Hand changes

--- custards.js
+++ custards.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
   $.ajax({
         url: '.',
         success: function (data) {
-            console.log(data);
+            //console.log(data);
         }
   });
 
